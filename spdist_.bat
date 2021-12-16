bd29a74e6c7dde36c757b5497b94ee1e97c94808
skaluzny <skaluzny@tibco.com>
2021-12-16 15:07:41 -0800

    Remove S-PLUS build files

--- spdist_.bat
+++
@@ -1,7 +0,0 @@
-echo d|xcopy %1\.Data %2\.Data /e /s
-echo d|xcopy %1\.Prefs %2\.Prefs /e /s
-echo d|xcopy %1\doc %2\doc /e /s
-copy %1\S.dll %2\S.dll
-copy %1\resample.chm %2\resample.chm
-copy %1\DESCRIPTION %2\DESCRIPTION
-copy %1\resample.pdf %2\resample.pdf
